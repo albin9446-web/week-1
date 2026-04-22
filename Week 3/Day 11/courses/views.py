468a8808abfa1dee871739de2188a89bc5993866
albin9446-web <albin9446@gmail.com>
2026-04-22 11:31:06 +0530

    Week 3 Day 13: Views and Templates

--- Week 3/Day 11/courses/views.py
+++ Week 3/Day 11/courses/views.py
@@ -1,7 +1,17 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
-    return HttpResponse("Welcome to Klaw Courses")
+    context = {
+        "title": "Klaw Courses",
+        "user_name": "Albin",
+        "courses": ["Python", "Django", "AI", "Web Development"]
+    }
+    return render(request, "courses/course_list.html", context)
+
 
 def detail_view(request, name):
-    return HttpResponse(f"You are viewing course: {name}")
+    return render(request, "courses/course_list.html", {
+        "title": f"Course: {name}",
+        "user_name": "Albin",
+        "courses": [name]
+    })
