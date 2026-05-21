cfb351864f9b56d92b1ff82164458d9fe6d67101
albin9446-web <albin9446@gmail.com>
2026-05-21 14:12:33 +0530

    Week 8 Day 36: Wishlist Toggle

---
+++ Week 8/day36_wishlist_toggle/android/app/src/main/kotlin/com/example/day36_wishlist_toggle/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day36_wishlist_toggle
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ Week 8/day36_wishlist_toggle/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const WishlistPage(),
+    );
+  }
+}
+
+class WishlistPage extends StatefulWidget {
+  const WishlistPage({super.key});
+
+  @override
+  State<WishlistPage> createState() => _WishlistPageState();
+}
+
+class _WishlistPageState extends State<WishlistPage> {
+
+  List<Map<String, dynamic>> items = [
+    {"name": "Laptop", "liked": false},
+    {"name": "Smartphone", "liked": false},
+    {"name": "Headphones", "liked": false},
+    {"name": "Keyboard", "liked": false},
+    {"name": "Mouse", "liked": false},
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Wishlist Toggle"),
+        backgroundColor: Colors.blue,
+      ),
+
+      body: ListView.builder(
+        itemCount: items.length,
+
+        itemBuilder: (context, index) {
+
+          return Card(
+            margin: const EdgeInsets.all(10),
+
+            child: ListTile(
+              title: Text(items[index]["name"]),
+
+              trailing: IconButton(
+
+                icon: Icon(
+                  items[index]["liked"]
+                      ? Icons.favorite
+                      : Icons.favorite_border,
+
+                  color: items[index]["liked"]
+                      ? Colors.red
+                      : Colors.grey,
+                ),
+
+                onPressed: () {
+
+                  setState(() {
+
+                    items[index]["liked"] =
+                        !items[index]["liked"];
+
+                  });
+                },
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
